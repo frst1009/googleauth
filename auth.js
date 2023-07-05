fee4480ff409eb6e6cb429779fbf3ee6b2084494
Ilaha E. Shukurova <ilahaesh@code.local>
2023-07-05 17:31:15 +0400

    first commit

---
+++ auth.js
@@ -0,0 +1,23 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth2').Strategy;
+
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+
+passport.use(new GoogleStrategy({
+  clientID: "555658076920-j47rdfnkckgap6cc14jcehksvasn9qta.apps.googleusercontent.com",
+  clientSecret: "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",
+  callbackURL: "http://localhost:5000/auth/google/callback",
+  passReqToCallback: true,
+},
+function(request, accessToken, refreshToken, profile, done) {
+  return done(null, profile);
+}));
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
